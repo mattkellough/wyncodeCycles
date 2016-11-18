0e5b8d149f85943a6a89ab58d191db970d51164e
Matt Kellough <mrk10a@gmail.com>
2016-11-17 22:29:15 -0500

    didnt work

--- app.rb
+++ app.rb
@@ -2,12 +2,6 @@ require 'sinatra'
 configure { set :server, :puma }
 
 
-
-get "/views/:file" do
-  file = params[:file] + "index.html"
-  if File.exists?(params[:file])
-    return File.open("views/" + file)
-  else
-   return "error"
-  end
+get '/' do
+  eb :index
 end
