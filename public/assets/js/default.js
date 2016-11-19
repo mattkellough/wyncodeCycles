51e24e4998b310b73f5deb1c32080645b5c19d12
Matt Kellough <mrk10a@gmail.com>
2016-11-18 22:24:04 -0500

    slider

--- public/assets/js/default.js
+++ public/assets/js/default.js
@@ -1,5 +1,58 @@
 $(document).ready(function(){
-  $('.mainLogo').mouseenter(function(){
-    $('.icon-redA').animate({left:'30px'});
+
+  function childrenPosition() {
+    var slideWidth = 1;
+    $('.sliderImages').find('img').each(function() {
+      var nextSlide = slideWidth + '00%';
+      $(this).next().css('left', nextSlide);
+      slideWidth++;
+    });
+  }
+
+  childrenPosition();
+
+  var sliderListItem = $('.sliderImages img'),
+  slideWidth = sliderListItem.width(),
+  sliderCount = sliderListItem.length;
+
+  console.log(sliderCount);
+
+  i = 0
+
+  function moveLeft() {
+    if (i!==0) {
+      sliderListItem.animate({
+        left: '+='+slideWidth
+      }, 200);
+      i--
+    }
+    console.log(i);
+  };
+
+  function moveRight() {
+    if (i!==5)  {
+      sliderListItem.animate({
+        left: '-='+slideWidth
+      }, 200);
+      i++
+    }
+    console.log(i);
+  };
+
+  $('a.prev').click(function () {
+    moveLeft();
+    clearInterval(int);
+    int = setInterval(moveRight,3000);
+    return false;
   });
+
+  $('a.next').click(function () {
+    moveRight();
+    clearInterval(int);
+    int = setInterval(moveRight,3000);
+    return false;
+  });
+
+  int = setInterval(moveRight,3000);
+
 });
